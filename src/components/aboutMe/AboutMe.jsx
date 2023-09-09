c55be0e85c485e4e73187b42f2697e2f37f51a6f
Omar-WD <129740652+Omar-WD@users.noreply.github.com>
2023-09-09 21:13:34 +0200

    fixing background imgs

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -12,7 +12,7 @@ export default function AboutMe() {
         <Col className="myImageDev">
           <Image fluid
             className="myImage"
-            src=".dist\omar.jpg"
+            src="./dist/omar.jpg"
             alt="Profile-Pic"
           />
         </Col>
