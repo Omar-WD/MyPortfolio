715d6b822df17858741220e2b27ba9e342b9a2fa
Omar-WD <129740652+Omar-WD@users.noreply.github.com>
2023-09-11 10:59:50 +0200

    fixing service icons

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Carousel, Card, Container, Row, Col } from 'react-bootstrap';
-import { FaHtml5, FaCss3Alt, FaCode,FaMoneyCheck } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaCode,FaMoneyCheck,FaDatabase} from 'react-icons/fa';
+import { HiMiniWrenchScrewdriver } from 'react-icons/hi2';
 import { AiFillGithub } from 'react-icons/ai';
+import { TbApi} from 'react-icons/tb';
+import { MdLanguage } from 'react-icons/md';
 import "./Services.css"
 
 const services = [
@@ -12,13 +15,13 @@ const services = [
         "Implementing responsive and user-friendly web designs using HTML5, CSS3, and Bootstrap. Creating interactive and dynamic web applications with JavaScript and React.js."
     },
     {
-      icon: FaCss3Alt,
+      icon: HiMiniWrenchScrewdriver,
       title: "Backend Development",
       description:
         "Building server-side applications and APIs using Node.js and Express for authorization and authentication."
     },
     {
-      icon: FaCode,
+      icon: FaDatabase,
       title: "Database Management",
       description:
         "Managing SQL and NoSQL databases, including ElephantSQL and MongoDB."
@@ -30,13 +33,13 @@ const services = [
         "Proficient in using Git and GitHub for version control and collaborative software development."
     },
     {
-      icon: FaCode,
+      icon: TbApi,
       title: "Restful APIs",
       description:
         "Designing, implementing, and consuming RESTful APIs for seamless data exchange between applications."
     },
     {
-      icon: FaCode,
+      icon: MdLanguage,
       title: "DevOps (Hosting, Deployment, Monitoring)",
       description:
         "Experience in the deployment, hosting, and monitoring of web applications to ensure optimal performance and availability."
